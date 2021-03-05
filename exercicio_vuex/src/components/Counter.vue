04d3609839ae4102e10e11ec0c764f26b2537574
Everton Cabral <evertoncab@gmail.com>
2021-03-04 22:35:39 -0300

    testando mutations, getters,actions

---
+++ exercicio_vuex/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="increment">Incrementar</button>
+    <button @click="decrement">Decrementar</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Counter",
+
+  computed: {
+    ...mapGetters(["counter"]),
+  },
+
+  methods: {
+    ...mapActions(["decrement", "increment"]),
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ exercicio_vuex/src/vuex.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vue.Store({
+  state: {
+    counter: 0,
+  },
+  mutations: {
+    decrement: (state) => state.counter--,
+    increment: (state) => state.counter++,
+  },
+  getters: {
+    counter: (state) => state.counter,
+  },
+  actions: {
+    decrement: ([commit]) => commit("decrement"),
+    increment: ([commit]) => {
+      setTimeout(() => {
+        commit("increment");
+      }, 2000);
+    },
+  },
+});
+
+export { store };
